c2484b1c608adbcf025075923ca598acd720e8e9
Parthibannr-Tech <parthibannr@outlook.com>
2025-02-22 22:24:33 +0530

    Added traceback

--- Run_Script.bat
+++ Run_Script.bat
@@ -1,6 +1,6 @@
 @echo off
-cd /d "%~dp0"  REM Change to the script's directory
-call .venv\Scripts\activate.bat
+cd /d "%~dp0"  REM
+call venv\Scripts\activate.bat
 python captcha_ocr.py
 deactivate
 pause
